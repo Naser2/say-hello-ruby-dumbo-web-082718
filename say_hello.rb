38022bbfde1ce1c5b1a75ce63647de61409b4095
Nasser Sanou <nassersanou23@gmail.com>
2018-08-19 01:41:12 +0000

    Done

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,8 @@
 # Build your say_hello method here
-def say_hello( name=" Ruby Programmer", arg="Hello ")
+def say_hello( name="Ruby Programmer", arg="Hello ")
 # name ="Ruby Programmer"
 puts "#{arg}"  + name + "!"
 end
 
  say_hello('Gabriela')
+ say_hello()
